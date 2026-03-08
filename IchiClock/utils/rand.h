d254ebcc016e44492652ca0b303719cd8953a506
Ichimaki Kasura <ichimakiKasura@gmail.com>
2026-03-08 16:45:38 +0800

    Modified the Adafruit Library by removing some functions that aren't needed. (saved only 6% of storage space)
    Memory space reduced 25%
    
    Code is somehow organized and cleaned up, lessen the nesting and
    spaghetti code. (I hope)

--- IchiClock/utils/rand.h
+++
@@ -1,9 +0,0 @@
-#pragma once
- // goofy lightweight random
-uint8_t rand(uint8_t num) {
-    static uint8_t seed = 42;
-    seed ^= seed << 3;
-    seed ^= seed >> 5;
-    seed ^= seed << 1;
-    return seed % num;
-}
